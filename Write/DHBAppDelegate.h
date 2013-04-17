6806bd19e052862be1e1d83d77bc909136de27d2
David Brunow <david.brunow@me.com>
2013-04-16 22:51:43 -0500

    Initial Commit

---
+++ Write/DHBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DHBAppDelegate.h
+//  Write
+//
+//  Created by David Brunow on 4/16/13.
+//  Copyright (c) 2013 David Brunow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DHBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Write/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Write
+//
+//  Created by David Brunow on 4/16/13.
+//  Copyright (c) 2013 David Brunow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DHBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DHBAppDelegate class]));
+    }
+}
